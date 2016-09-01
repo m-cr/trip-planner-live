ffdf8e8cca43a589e0dacbee60e4432ea2ea1746
Michael Crowley <miketcrowley@gmail.com>
2016-09-01 18:26:10 -0400

    only one hotel, and made remove buttons remove elements from dom

--- public/js/main.js
+++ public/js/main.js
@@ -48,6 +48,8 @@ $(
     activity: '/images/star-3.png'
   };
 
+  var gMarkers = [];
+
   function drawMarker (type, coords) {
     var latLng = new google.maps.LatLng(coords[0], coords[1]);
     var iconURL = iconURLs[type];
@@ -55,68 +57,88 @@ $(
       icon: iconURL,
       position: latLng
     });
+    console.log(marker);
+    gMarkers.push(marker);
     marker.setMap(currentMap);
+  }
+
+  function removeMarker (type, coords) {
+    console.log('called');
+    var latLng = google.maps.LatLng(coords[0], coords[1]);
+    var iconURL = iconURLs[type];
+    var marker = new google.maps.Marker({
+      icon: iconURL,
+      position: latLng
+    });
+    for(var i = 0; i < gMarkers.length; i ++){
+      console.log(gMarkers[i]==marker);
+      if(marker == gMarkers[i]){
+        console.log('in if in for');
+        gMarkers[i].setMap(null);
+      }
+    }
   }
 
   // drawMarker('hotel', [40.705137, -74.007624]);
   // drawMarker('restaurant', [40.705137, -74.013940]);
   // drawMarker('activity', [40.716291, -73.995315]);
-$( document ).ready(function() {
-    
+  $( document ).ready(function() {
+      hotels.forEach(function(hotel){
+          $('#hotel-choices').append('<option>'+hotel.name+'</option>');
+      });
 
-    hotels.forEach(function(hotel){
-        $('#hotel-choices').append('<option>'+hotel.name+'</option>');
-    });
+      activities.forEach(function(activity){
+          $('#activity-choices').append('<option>'+activity.name+'</option>');
+      });
 
-    activities.forEach(function(activity){
-        $('#activity-choices').append('<option>'+activity.name+'</option>');
-    });
+      restaurants.forEach(function(restaurant){
+          $('#restaurant-choices').append('<option>'+restaurant.name+'</option>');
+      });
 
-    restaurants.forEach(function(restaurant){
-        $('#restaurant-choices').append('<option>'+restaurant.name+'</option>');
-    });
+      $("button[data-action='add']").on('click',function(){
+        var type=$(this).prev().data('type');
+        var value=$(this).prev().val();
+        var location;
+        var itenaryItem='<div class="itinerary-item">'+
+                  '<span class="title">'+value+'</span>'+
+                  '<button class="btn btn-xs btn-danger remove btn-circle">x</button>'+
+                '</div>';
+        switch(type){
+          case 'hotel':
+            location=hotels.filter(function(hotel){
+                return hotel.name==value;
+            })[0].place.location;
+            if($($('#itinerary .list-group')[0]).children().length != 0)
+              break;
+            $($('#itinerary .list-group')[0]).append(itenaryItem);
 
-    $("button[data-action='add']").on('click',function(){
-
-      var type=$(this).prev().data('type');
-      var value=$(this).prev().val();
-      var location;
-
-      var itenaryItem='<div class="itinerary-item">'+
-                '<span class="title">'+value+'</span>'+
-                '<button class="btn btn-xs btn-danger remove btn-circle">x</button>'+
-              '</div>';
-
-
-
-      switch(type){
-        case 'hotel':
-          location=hotels.filter(function(hotel){
-              return hotel.name==value;
+          break;
+          case 'restaurant':
+           location=restaurants.filter(function(restaurant){
+                return restaurant.name==value;
+            })[0].place.location;
+            $($('#itinerary .list-group')[1]).append(itenaryItem);
+          break;
+          case 'activity':
+           location=activities.filter(function(activity){
+                return activity.name==value;
+            })[0].place.location;
+            $($('#itinerary .list-group')[2]).append(itenaryItem);
+          break;
+          default:
+        }
+        drawMarker(type, location);
+        removeMarker(type,location);
+      $("#itinerary .itinerary-item button").on('click',function(){
+        // console.log($(this).prev().val());
+        var type='hotel';
+        console.log(type);
+        location = hotels.filter(function(hotel){
+            return hotel.name==value;
           })[0].place.location;
-          $($('#itinerary .list-group')[0]).append(itenaryItem);
-
-        break;
-        case 'restaurant':
-         location=restaurants.filter(function(restaurant){
-              return restaurant.name==value;
-          })[0].place.location;
-          $($('#itinerary .list-group')[1]).append(itenaryItem);
-        break;
-        case 'activity':
-         location=activities.filter(function(activity){
-              return activity.name==value;
-          })[0].place.location;
-          $($('#itinerary .list-group')[2]).append(itenaryItem);
-        break;
-        default:
-
-      }
-     // marker.setMap(null);
-     
-      drawMarker(type, location);
-
+        $(this).parent().empty();
+        removeMarker(type, location);
+      });
     });
-
-});
+  });
 });
